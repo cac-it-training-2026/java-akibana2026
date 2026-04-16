497bd15fe67353b5b3be96d2ffec68aeb2ac41a3
akibana <akibana@C010422871.zipangu.cac.co.jp>
2026-04-16 10:57:24 +0900

    question03_01 コミットプッシュ

--- java_comprehension_exercises_volume1/src/basic/question03/Question03_1.java
+++ java_comprehension_exercises_volume1/src/basic/question03/Question03_1.java
@@ -7,8 +7,14 @@ package basic.question03;
 public class Question03_1 {
 
 	public static void main(String[] args) {
+		//1
+		int price = 30;
+		//2
+		int totalBudget = 300;
+		//3
+		int quantity = totalBudget / price;
+		//4
+		System.out.println("商品の購入数は" + quantity + "個です。");
 
-		// TODO:ここに実装
-		
 	}
 }
