8022210a87a2e1ab3002b29225bb91d835ab1874
akibana <akibana@C010422871.zipangu.cac.co.jp>
2026-04-16 14:14:21 +0900

    question07_04 コミットプッシュ

--- java_comprehension_exercises_volume1/src/advanced/question07/Question07_4.java
+++ java_comprehension_exercises_volume1/src/advanced/question07/Question07_4.java
@@ -1,6 +1,8 @@
 package advanced.question07;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Question07_4 {
 
@@ -13,7 +15,48 @@ public class Question07_4 {
 		// 購入数（変更不可）
 		int[] amounts = new int[5];
 
-		// TODO: 実装ここから
+		//1
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String stocks_str;
+		String amounts_str;
+		int prices_sum = 0;
+
+		int[] stocks = new int[5];
+
+		for (int i = 0; i < 5; i++) {
+			System.out.print(itemNames[i] + "の在庫数:>");
+			stocks_str = reader.readLine();
+			stocks[i] = Integer.parseInt(stocks_str);
+		}
+
+		for (int i = 0; i < 5;) {
+			System.out.print(itemNames[i] + "の購入数:>");
+			amounts_str = reader.readLine();
+			amounts[i] = Integer.parseInt(amounts_str);
+			if (amounts[i] > stocks[i]) {
+				System.out.println("在庫を超えています。もう一度入力してください。");
+				continue;
+			}
+			stocks[i] -= amounts[i];
+			i++;
+		}
+		for (int i = 0; i < 5; i++) {
+			System.out.println(itemNames[i] + ":購入" + amounts[i] + "個");
+			System.out.println("在庫" + stocks[i]);
+			prices_sum += (amounts[i] * prices[i]);
+		}
+
+		System.out.println("割引前合計:" + prices_sum + "円");
+
+		//1
+		if (prices_sum >= 5000) {
+			prices_sum *= 0.9;
+			System.out.println("割引適用（10%OFF）:" + prices_sum + "円");
+		}
+
+		//2
+		prices_sum *= 1.1;
+		System.out.println("税込合計:" + prices_sum + "円");
 
 	}
 
