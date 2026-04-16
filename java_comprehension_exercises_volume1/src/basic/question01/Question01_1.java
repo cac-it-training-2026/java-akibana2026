acdce1180acc5e10fd89e7fc8bceabf3a910897b
akibana <akibana@C010422871.zipangu.cac.co.jp>
2026-04-16 10:24:16 +0900

    question01

--- java_comprehension_exercises_volume1/src/basic/question01/Question01_1.java
+++ java_comprehension_exercises_volume1/src/basic/question01/Question01_1.java
@@ -7,7 +7,21 @@ package basic.question01;
 public class Question01_1 {
 
 	public static void main(String[] args) {
-		更新確認
+		//①
+		int price = 120;
+		//②
+		String item = "ノート";
+		//③
+		System.out.println(price);
+		System.out.println(item);
+		//④
+		price = 200;
+		//⑤
+		item = "鉛筆";
+		//⑥
+		System.out.println(price);
+		System.out.println(item);
+
 		// TODO:ここに実装
 
 	}
