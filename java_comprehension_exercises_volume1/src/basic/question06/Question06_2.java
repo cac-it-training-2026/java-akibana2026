bdf93428325ad1e3c54d7730bf3cfedb3b7f59fa
akibana <akibana@C010422871.zipangu.cac.co.jp>
2026-04-16 12:03:08 +0900

    question06_02 コミットプッシュ

--- java_comprehension_exercises_volume1/src/basic/question06/Question06_2.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_2.java
@@ -6,7 +6,11 @@ public class Question06_2 {
 
 	public static void main(String[] args) throws IOException {
 
-		// TODO:ここに実装
-		
+		//1
+		String[] studentName = { "かなこ", "ゆうた", "あい", "はるひ", "かずき" };
+		//2
+		for (String name : studentName) {
+			System.out.println(name);
+		}
 	}
 }
